1388762b5783e2385ed34c5d08ec7a1728aea3fb
Dennis Schmidt <smgdennis@online.de>
2015-02-01 21:17:22 +0100

    Mit Kommentaren

--- src/tud/ke/ml/project/classifier/NearestNeighbor.java
+++ src/tud/ke/ml/project/classifier/NearestNeighbor.java
@@ -29,7 +29,10 @@ public class NearestNeighbor extends ANearestNeighbor {
 	
 	@Override
 	protected Object vote(List<Pair<List<Object>, Double>> subset) {
+		//Get all unweighted Votes
 		Map<Object, Double> unweightedVotes =getUnweightedVotes(subset);
+		
+		//Return winner class
 		return getWinner(unweightedVotes);
 	}
 	@Override
@@ -41,16 +44,27 @@ public class NearestNeighbor extends ANearestNeighbor {
 	@Override
 	protected Map<Object, Double> getUnweightedVotes(
 			List<Pair<List<Object>, Double>> subset) {
+		
+		//Map for the unweighted votes results
 		Map<Object, Double> result = new HashMap<Object,Double>();
+		
+		//Iterate over all instances in the subset
 		for(Pair<List<Object>, Double> instance: subset){
+			
+			//Get the class attribut
 			Object classAttribut = instance.getA().get(getClassAttribute());
+			
+			//Count the votes
 			double number = 0.0;
 			if(!result.containsKey(classAttribut)){
 				number = 1.0;
 			}
 			else{
 				number = result.get(classAttribut)+1.0;
+				result.remove(classAttribut);
 			}
+			
+			//Update the Result
 			result.put(classAttribut, number);
 		}
 		return result;
@@ -63,8 +77,11 @@ public class NearestNeighbor extends ANearestNeighbor {
 	}
 	@Override
 	protected Object getWinner(Map<Object, Double> votesFor) {
+		//Data of the winner
 		double max = 0;
 		Object winnerClass = null;
+		
+		//Search the winner
 		for(Entry entry : votesFor.entrySet()){
 			if ((double)entry.getValue() >= max){
 				max = (double) entry.getValue();
@@ -102,10 +119,12 @@ public class NearestNeighbor extends ANearestNeighbor {
 			}
 		};
 		
+		//Sort the list and get the first k instances
 		Collections.sort(result, comp);
 		
 		int to = Math.min(result.size(),getkNearest());
 		
+		//Return the list of the first k instances
 		if (to > 1){
 			return result.subList(0, to-1);
 		}
@@ -121,8 +140,13 @@ public class NearestNeighbor extends ANearestNeighbor {
 	@Override
 	protected double determineManhattanDistance(List<Object> instance1,
 			List<Object> instance2) {
+		//Set distance to 0
 		double result = 0;
+		
+		//Calculate the Distance
 		for(int i = 0; i<instance1.size()-1;i++){
+			
+			//If it is a String
 			if(instance1.get(i) instanceof String){
 				if(!instance1.get(i).equals(instance2.get(i))){
 					result++;	
@@ -130,6 +154,7 @@ public class NearestNeighbor extends ANearestNeighbor {
 
 			}
 			else{
+				//If it is numeric
 				result = result + Math.abs(((double) instance1.get(i)- (double) instance2.get(i)));	
 			}
 		}
